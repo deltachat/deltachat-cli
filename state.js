4ae8db5d458b71194aad4191a06137858f99f3ae
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-08-09 03:54:22 +0200

    Fix standard

--- state.js
+++ state.js
@@ -41,8 +41,8 @@ class ChatMessage {
 const toDate = ts => {
   const date = new Date(ts * 1000)
   const hours = date.getHours()
-  const minutes = "0" + date.getMinutes()
-  const seconds = "0" + date.getSeconds()
+  const minutes = '0' + date.getMinutes()
+  const seconds = '0' + date.getSeconds()
   return `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`
 }
 
