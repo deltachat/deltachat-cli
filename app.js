5123a0ca6bfe51ffda0c336764027f9d48a6d7bd
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-08-09 18:45:08 +0200

    Move out Commander to commander.js

--- app.js
+++ app.js
@@ -1,73 +1,8 @@
 const neatLog = require('neat-log')
 const chalk = require('chalk')
 const State = require('./state')
+const Commander = require('./commander')
 const Layout = require('./layout')
-
-class Commander {
-  constructor (state) {
-    this.state = state
-
-    this.commands = {
-      help: {
-        help: {
-          syntax: 'help [<command>]',
-          description: 'Displays the documentation for the given command.',
-          examples: [ '/help', '/help help' ]
-        },
-        run: arg => {
-          if (typeof arg === 'string') {
-            // Help about a specific command
-            const cmd = this.commands[arg]
-            if (cmd && cmd.help) {
-              this.status(renderHelp(cmd.help))
-            } else {
-              this.status(`No help for ${arg}`)
-            }
-          } else {
-            // List all available commands
-            const keys = Object.keys(this.commands)
-            this.status('deltax commands:\n' + keys.join(' '))
-          }
-        }
-      }
-    }
-  }
-
-  onEnter (line) {
-    if (line[0] !== '/') {
-      return this.state.onEnter(line)
-    }
-
-    line = line.slice(1).split(' ')
-    const cmd = this.commands[line[0]]
-    const args = line.slice(1)
-
-    if (cmd && typeof cmd.run === 'function') {
-      cmd.run.apply(this, args)
-    } else {
-      this.status(`Unknown command: ${line[0]}`)
-    }
-  }
-
-  status (line) {
-    this.state.appendToStatusPage(line)
-  }
-
-  onTab () {
-    // TODO handle auto complete and modify this.state.input
-  }
-}
-
-function renderHelp (help) {
-  return [
-    ` \n${chalk.bold('Syntax:')}\n \n`,
-    `  ${help.syntax}\n \n`,
-    `${chalk.bold('Description:')}\n \n`,
-    `  ${help.description}\n \n`,
-    `${chalk.bold('Examples:')}\n \n`,
-    `${help.examples.map(ex => '  ' + ex).join('\n')}`
-  ].join('')
-}
 
 class App {
   constructor (rc, dc) {
---
+++ commander.js
@@ -0,0 +1,69 @@
+const chalk = require('chalk')
+
+class Commander {
+  constructor (state) {
+    this.state = state
+
+    this.commands = {
+      help: {
+        help: {
+          syntax: 'help [<command>]',
+          description: 'Displays the documentation for the given command.',
+          examples: [ '/help', '/help help' ]
+        },
+        run: arg => {
+          if (typeof arg === 'string') {
+            // Help about a specific command
+            const cmd = this.commands[arg]
+            if (cmd && cmd.help) {
+              this.status(renderHelp(cmd.help))
+            } else {
+              this.status(`No help for ${arg}`)
+            }
+          } else {
+            // List all available commands
+            const keys = Object.keys(this.commands)
+            this.status('deltax commands:\n' + keys.join(' '))
+          }
+        }
+      }
+    }
+  }
+
+  onEnter (line) {
+    if (line[0] !== '/') {
+      return this.state.onEnter(line)
+    }
+
+    line = line.slice(1).split(' ')
+    const cmd = this.commands[line[0]]
+    const args = line.slice(1)
+
+    if (cmd && typeof cmd.run === 'function') {
+      cmd.run.apply(this, args)
+    } else {
+      this.status(`Unknown command: ${line[0]}`)
+    }
+  }
+
+  status (line) {
+    this.state.appendToStatusPage(line)
+  }
+
+  onTab () {
+    // TODO handle auto complete and modify this.state.input
+  }
+}
+
+function renderHelp (help) {
+  return [
+    ` \n${chalk.bold('Syntax:')}\n \n`,
+    `  ${help.syntax}\n \n`,
+    `${chalk.bold('Description:')}\n \n`,
+    `  ${help.description}\n \n`,
+    `${chalk.bold('Examples:')}\n \n`,
+    `${help.examples.map(ex => '  ' + ex).join('\n')}`
+  ].join('')
+}
+
+module.exports = Commander
